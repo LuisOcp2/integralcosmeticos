c4f1b51331d9764477baa3b792561a3a0fea5828
LuisOcp2 <luisocampo20022015@gmail.com>
2026-03-27 13:14:21 -0500

    feat(sync): simplificar wiring del modulo de sincronizacion

--- apps/backend/src/modules/sync/sync.module.ts
+++ apps/backend/src/modules/sync/sync.module.ts
@@ -1,6 +1,5 @@
 import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
-import { ScheduleModule } from '@nestjs/schedule';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SyncController } from './sync.controller';
 import { SyncProcessor } from './sync.processor';
@@ -8,11 +7,7 @@ import { SyncLog } from './entities/sync-log.entity';
 import { SyncService } from './sync.service';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([SyncLog]),
-    BullModule.registerQueue({ name: 'sync' }),
-    ScheduleModule,
-  ],
+  imports: [TypeOrmModule.forFeature([SyncLog]), BullModule.registerQueue({ name: 'sync' })],
   controllers: [SyncController],
   providers: [SyncService, SyncProcessor],
   exports: [SyncService],
